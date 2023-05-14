adae7d11755327bdebaeb505396c581f23125de0
Mahesh Nilkanth Londal <101565585+Maheshlondal@users.noreply.github.com>
2023-05-14 16:36:54 +0530

    E_commerce project
    done

---
+++ Project/Ecommerce-Backend/src/main/java/ecommerce/entities/Admin.java
@@ -0,0 +1,41 @@
+package ecommerce.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private String email;
+	private String pwd;
+	private String uname;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	@Override
+	public String toString() {
+		return "Admin [userid=" + email + ", pwd=" + pwd + ", uname=" + uname + "]";
+	}
+	
+}
